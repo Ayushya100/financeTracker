bb170f74fe6bd089e2057506b72eb83248008a55
Ayushya <jaiswalayushya@gmail.com>
2023-10-02 16:54:05 +0530

    Reverted validateToken API method change

--- accountManagementServices/routes/validateToken.js
+++ accountManagementServices/routes/validateToken.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const services = require('../services');
 
 // API
-router.get('/', async(req, res) => {
+router.post('/', async(req, res) => {
     try {
         let payload = req.body;
         const payloadValidationResult = await services.validatePayload(payload, 'validate-token');
